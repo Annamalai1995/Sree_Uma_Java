ed59858533509de099ba9f6ceda46d681564fae3
Annamalai1995 <s.annamalai1995@gmail.com>
2024-08-08 11:35:29 +0530

    Datatypes

---
+++ src/main/java/Fundamental/Variables/Demo_Datatypes.java
@@ -0,0 +1,24 @@
+package Fundamental.Variables;
+
+public class Demo_Datatypes {
+
+    public static void main(String[] args) {
+        System.out.println("----------BIODATA---------");
+
+        String Name="Gowthami Priya";
+        int Age=28;
+        float Height=5.1f;
+        String Place="salem";
+        char Gender='F';
+        long contact_no=85745899542454l;
+        System.out.println("Name is:"+Name);
+        System.out.println("Age is:"+Age);
+        System.out.println("Height Is:"+Height);
+        System.out.println("Place :"+Place);
+        System.out.println("Gender:"+Gender);
+        System.out.println("Contact number:"+contact_no);
+
+
+
+    }
+}
